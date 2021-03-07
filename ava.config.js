239bc3e47e8b56292f8e1aa1f5854293ced2980e
gosho-kazuya <ketsume0211@gmail.com>
2021-03-08 01:02:59 +0900

    add first class

---
+++ ava.config.js
@@ -0,0 +1,5 @@
+export default {
+  extensions: ['ts', 'tsx'],
+  require: ['ts-node/register/transpile-only'],
+  files: ['src/**/*.test.ts'],
+}
---
+++ src/GraphORM.test.ts
@@ -0,0 +1,6 @@
+import test from 'ava'
+import { GraphORM } from './GraphORM'
+
+test('GraphORM', (t) => {
+  t.truthy(GraphORM)
+})
